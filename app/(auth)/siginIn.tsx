747d1dffc2fa7716949db36922ae6892f4c75a4d
Evanio Tech <evanioshark@gmail.com>
2025-11-17 09:42:12 -0300

    style page signin

--- app/(auth)/siginIn.tsx
+++ app/(auth)/siginIn.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 import React from 'react'
 import { useRouter } from 'expo-router'
 
@@ -8,9 +8,25 @@ const siginIn = () => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.sectionForm}>
+            <TextInput 
+            placeholder="Digite seu QRA" 
+            placeholderTextColor={"black"}
+            style={styles.input} />
+            <TextInput 
+            placeholder="Digite seu Email" 
+            placeholderTextColor={"black"}
+            style={styles.input} />
+            <TextInput 
+            placeholder="Digite sua senha" 
+            placeholderTextColor={"black"}
+            secureTextEntry={true}
+             style={styles.input} />
+
+      </View>
       <Text>siginIn</Text>
       <TouchableOpacity onPress={() => router.push('siginUp')}>
-        <Text>Go to Sigin Up</Text>
+        <Text style={styles.textButton}>Go to Sigin Up</Text>
       </TouchableOpacity>
     </View>
   )
@@ -21,7 +37,29 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#eeebeb',
+        backgroundColor: '#0f0101',
+    },
+    sectionForm: {
+        width: '80%',
+        height: '50%',
+        backgroundColor: '#0f0101',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    input: {
+        width: '80%',
+        height: 50,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginVertical: 10,
+    },
+    textButton: {
+        color: '#00ffff',
+        fontSize: 18,
+        fontWeight: 'bold',
     }
 })
 
